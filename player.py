6835a1f04d27f136fe7ea12831423244822f4874
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-03-25 23:24:44 +0800

    added more functionality

--- player.py
+++ player.py
@@ -3,14 +3,40 @@ import pygame
 import random
 import threading
 import copy
+import math
+import time
 from mutagen.mp3 import MP3
 
 lastSongs = []
+paused = False
+volume = 0.5
 
-def stopMusic():
-    input("Press Enter to stop the music.\n")
-    pygame.mixer.music.stop()
-    pygame.mixer.quit()
+def control():
+    while 1:
+        res = input("Press Enter to stop the music or enter command.\n")
+        if res == '':
+            pygame.mixer.music.stop()
+            pygame.mixer.quit()
+            print("Stopped MP3 player")
+            return
+        elif res == 'p':
+            if not paused:
+                print("Paused MP3 player")
+                paused = True
+                pygame.mixer.music.pause()
+            else:
+                print("Unpaused MP3 player")
+                paused = False
+                pygame.mixer.music.unpause()
+        elif res == '+':
+            volume = round(min(volume + 0.1, 1), 1)
+            print(f"Increased Volume to {volume}")
+            pygame.mixer.music.set_volume(volume)
+        elif res == '-':
+            volume = round(max(volume - 0.1, 0.1), 1)
+            print(f"Decreased Volume to {volume}")
+            pygame.mixer.music.set_volume(volume)
+        
 
 def removeArray(arr, toRemove):
     for element in toRemove:
@@ -41,9 +67,10 @@ def MP3Player(playlistName='playlist'):
         print("No songs found in playlist. Check that all files are .mp3")
         return
 
-    quitPlayer = threading.Thread(target=stopMusic)
-    quitPlayer.start()
+    controlThread = threading.Thread(target=control)
+    controlThread.start()
 
+    print("Started MP3 player")
     while (1):
         song = chooseSong(playlist)
         songPath = os.path.join(playlistFolder, song)
@@ -52,9 +79,14 @@ def MP3Player(playlistName='playlist'):
         duration = audio.info.length
 
         print(f"Now playing: {song}")
+        startTime = time.time()
         pygame.mixer.music.load(songPath)
         pygame.mixer.music.play()
-        pygame.time.wait(int(duration * 1000))
+        while time.time()-startTime <= duration:
+            pygame.time.wait(1000)
+            if paused:
+                duration += 1000
         pygame.mixer.music.stop()
 
-MP3Player()
+print('loading MP3 player')
+MP3Player('playlist')
